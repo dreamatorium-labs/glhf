aa66a2d7e06389e494bb7082851ee7fc85bff9ca
FME849 <1414439@gmail.com>
2024-01-04 16:49:39 +0700

    feat: add GameCustomizePage

---
+++ apps/landing/pages/game-create/customize/index.tsx
@@ -0,0 +1,71 @@
+import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@walless/gui';
+import Layout from 'components/Layout';
+import ProgressIndicator from 'components/ProgressIndicator';
+import { useRouter } from 'next/router';
+import { gameState } from 'state/game';
+import { bangers, buttonStyle, montserrat } from 'utils/style';
+import { useSnapshot } from 'valtio';
+
+export const GameCustomizePage = () => {
+	const router = useRouter();
+	const { banner } = useSnapshot(gameState);
+
+	const handleContinue = () => {
+		router.push('/game-create/customize');
+	};
+
+	useEffect(() => {}, []);
+
+	return (
+		<Layout style={styles.container}>
+			<View style={styles.titleContainer}>
+				<ProgressIndicator steps={4} currentStep={2} />
+				<View style={styles.headContainer}>
+					<Text style={styles.title}>CUSTOMIZE YOUR GAME</Text>
+					<Button
+						title="Continue"
+						style={[buttonStyle.button, styles.continueBtn]}
+						titleStyle={buttonStyle.title}
+						onPress={handleContinue}
+					/>
+				</View>
+				<Text style={styles.subTitle}>
+					{`Personalize characters, environments, and features to match your unique vision.\nExplore customization options to make your game stand out.`}
+				</Text>
+			</View>
+		</Layout>
+	);
+};
+
+export default GameCustomizePage;
+
+const styles = StyleSheet.create({
+	container: {
+		gap: 40,
+	},
+	headContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'flex-start',
+	},
+	titleContainer: {
+		gap: 15,
+	},
+	title: {
+		color: 'white',
+		fontSize: 60,
+		fontFamily: bangers.style.fontFamily,
+	},
+	subTitle: {
+		color: 'white',
+		fontSize: 16,
+		fontFamily: montserrat.style.fontFamily,
+		fontWeight: '400',
+		lineHeight: 25,
+	},
+	continueBtn: {
+		paddingVertical: 10,
+	},
+});
